e163389e83df1736c8c93f7c460532fdbfc68e75
Jacob Ogren <jogren0044@gmail.com>
2019-09-26 07:37:48 -0600

    Add getAllMessages and addMessage functions

--- server.js
+++ server.js
@@ -12,39 +12,37 @@ let messages = [
   { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
 ];
 
-server.on('request', (request, response) => {
-  console.log(request)
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World\n');
+const getAllMessages = (response) => {
+  response.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  response.write(JSON.stringify(messages));
   response.end();
+}
+
+const addMessage = (newMessage, response) => {
+  const allMessages = [...messages, newMessage]
+  response.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  response.write(JSON.stringify(allMessages));
+  response.end();
+}
+
+server.on('request', (request, response) => {
+  if (request.method === 'GET') {
+    getAllMessages(response);
+  }
+
+  else if (request.method === 'POST') {
+    let newMessage = { 'id': new Date() };
+
+    request.on('data', (data) => {
+      newMessage = Object.assign(newMessage, JSON.parse(data));
+    });
+
+    request.on('end', () => {
+      addMessage(newMessage, response);
+    });
+  }
 });
-
-// server.on('request', (request, response) => {
-//   if (request.method === 'GET') {
-//     getAllMessages(response);
-//   }
-
-//   else if (request.method === 'POST') {
-//     let newMessage = { 'id': new Date() };
-
-//     request.on('data', (data) => {
-//       newMessage = Object.assign(newMessage, JSON.parse(data));
-//     });
-
-//     request.on('end', () => {
-//       addMessage(newMessage, response);
-//     });
-//   }
-// });
-
-// function getAllMessages(response) {
-//   response.writeHead(200, { 'Accept': 'application / json' });
-//   response.write(messages);
-//   response.end();
-// }
-
-// function addMessage(newMessage, response) {
-//   response.writeHead(200, { 'Accept': 'application / json' });
-//   response.write([...messages, newMessage]);
-//   response.end();
-// }
